41e563cb35d0bb1cadf4bf7f34b307d4cb71c014
RTNGLOBAL <risetonet@hotmail.com>
2025-03-23 16:04:06 +0500

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://192.168.137.214:5000';
+const API_URL = 'https://tourprism-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
