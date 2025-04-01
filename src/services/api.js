7a507e7815618315328036b314f3781ddc10cc73
web-source-dev <muhammadnouman72321@gmail.com>
2025-04-01 10:23:19 +0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://tourprism-backend.onrender.com';
+const API_URL = 'https://tourprism-backend-w5c1.onrender.com';
 // const API_URL = 'http://localhost:5000';
 const api = axios.create({
   baseURL: API_URL,
